82b2569b4c109518667f2d3185797c72b641e54c
mohammad-shehadeh <mshmohamad@iCloud.com>
2025-02-24 11:23:46 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,26 @@
+import { 
+  initializeApp 
+} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { 
+  getAuth, 
+  signInWithEmailAndPassword, 
+  onAuthStateChanged, 
+  signOut 
+} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { 
+  getFirestore, 
+  doc, 
+  setDoc, 
+  getDoc 
+} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+
+// إعداد Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBP2bnt1DNNUO0dFtfiIovxMG-NM6yXPMM",
+  authDomain: "aasa-8a079.firebaseapp.com",
+  projectId: "aasa-8a079",
+  storageBucket: "aasa-8a079.appspot.com",
+  messagingSenderId: "849330713582",
   appId: "1:849330713582:web:7a11b11ebdb5cdcc8b14ff"
 };
 
